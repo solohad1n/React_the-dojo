e75f0fe2756691f4878b9bcd04f27ff577ab384b
solohad1n <fox663399@gmail.com>
2022-06-30 12:47:01 +0600

    added entry and exit condition

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Temple from '../../assets/temple.svg'
+import { useAuthContext } from '../../Hooks/useAuthContext'
 import { useLoginout } from '../../Hooks/useLoginout'
 import './styles.css'
 
 const Navbar = () => {
+  const { user } = useAuthContext()
   const { logout, error, isPending } = useLoginout()
   return (
     <nav className='navbar'>
@@ -13,17 +15,25 @@ const Navbar = () => {
           <img src={Temple} alt="dojo logo" />
           <span>The Dojo</span>
         </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-        <li>
-          {!isPending && <button onClick={logout} className='btn'>Logout</button>}
-          {isPending && <button className='btn'>Loading...</button>}
-          {error && <div className='error'>{error}</div>}
-        </li>
+        {!user && (
+          <>
+            {" "}
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <li>
+            {!isPending && <button onClick={logout} className='btn'>Logout</button>}
+            {isPending && <button className='btn'>Loading...</button>}
+            {error && <div className='error'>{error}</div>}
+          </li>
+        )}
       </ul>
     </nav>
   )
